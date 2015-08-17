e6a8a2b2b750ffeb2294ec432dd0b53f5abf65d7
anjanaraveendra <anjana@cognitiveclouds.com>
2015-08-17 21:18:20 +0530

    Cod eYo :a:

---
+++ app/models/ability.rb
@@ -0,0 +1,43 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.principal?
+      can :manage, :all
+    elsif user.teacher?
+      can :read, :all
+    elsif user.student?
+    end
+  end
+
+   def set_default_role
+    self.role ||= :student
+   end
+
+    # Define abilities for the passed in user here. For example:
+    #
+    #   user ||= User.new # guest user (not logged in)
+    #   if user.admin?
+    #     can :manage, :all
+    #   else
+    #     can :read, :all
+    #   end
+    #
+    # The first argument to `can` is the action you are giving the user
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on.
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, :published => true
+    #
+    # See the wiki for details:
+    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+end
