863f4b8ca23eda99b0f2d7792874004b56c8f570
anibha88 <anibha88@gmail.com>
2015-08-18 11:03:26 +0530

    Basic user navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   devise_for :users
+
+  root to: "static_pages#home"
+
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
